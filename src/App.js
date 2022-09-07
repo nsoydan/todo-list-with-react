6cb4c5eaefda2f389426a1314c63507fe65be112
nihat soydan <nsoydan@gmail.com>
2022-09-07 23:54:00 +0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Home from './component/home/home.component';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/todo/todo.component.jsx
@@ -0,0 +1,12 @@
+
+
+const Home=()=>{
+    return(
+        <div>
+            <input type="text" />
+        </div>
+    );
+};
+
+
+export default Home;
